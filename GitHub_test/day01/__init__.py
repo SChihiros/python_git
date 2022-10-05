33fbb37d30de5e0b11089f07d58e210c49999c6c
shenyu <2337505963@qq.com>
2022-10-05 15:23:31 +0800

    首次提交工程

---
+++ GitHub_test/day01/__init__.py
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+"""
+__init__.py.py
+Created on 2022/10/5 15:20
+Copyright (c) 2022/10/5
+@author: shenyu
+"""
---
+++ GitHub_test/day01/m1.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+"""
+m1.py
+Created on 2022/10/5 15:21
+Copyright (c) 2022/10/5
+@author: shenyu
+"""
+
+a = 1
+b = 2
+c = a + b
+print(c)
