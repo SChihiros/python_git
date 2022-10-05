d7f5cff74b7691917c61e6eed18ccb7132b8b273
shenyu <2337505963@qq.com>
2022-10-05 15:28:05 +0800

    修改m1，增加m2

--- GitHub_test/day01/m1.py
+++ GitHub_test/day01/m1.py
@@ -5,8 +5,11 @@ Created on 2022/10/5 15:21
 Copyright (c) 2022/10/5
 @author: shenyu
 """
+from day01 import m2
 
 a = 1
 b = 2
 c = a + b
 print(c)
+
+m2.add(a,b)
---
+++ GitHub_test/day01/m2.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+"""
+m2.py
+Created on 2022/10/5 15:25
+Copyright (c) 2022/10/5
+@author: shenyu
+"""
+
+def add(a,b):
+    print(a+b)
